b8214ed67ae5eaa9d35dd34dfd7f626c1e395330
Caio Alves <caioalv7@gmail.com>
2023-02-07 17:46:27 -0300

    feat: create get and put requests

--- NodeJS/new-api/src/server.js
+++ NodeJS/new-api/src/server.js
@@ -1,2 +1,30 @@
 const express = require('express');
+let books = require('./data/books.json');
+let authors = require('./data/authors.json');
+
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.get('/books', (req, res) => {
+
+    for (const book of books) {
+        book.author = authors.find(author => author.id === book.author_id)
+    }
+
+    const infoBook = {
+        total: books.length,
+        books,
+    };
+
+    res.send(infoBook);
+});
+
+
+app.post('/books', (req, res) => {
+    books.push(req.body);
+    res.send("Post success!");
+});
+
+app.listen(3000, () => console.log("http://localhost:3000"));
